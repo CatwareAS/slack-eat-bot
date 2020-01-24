5d7dd9d96c13f12a2e0e3b99772c93d12a78fcc4
Maksym.Pecheniuk <uamaxua@gmail.com>
2020-01-24 17:47:36 +0200

    Initial commit

---
+++ src/main/java/com/slack/eatapp/EatAppApplication.java
@@ -0,0 +1,13 @@
+package com.slack.eatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/slack/eatapp/controller/SlackController.java
@@ -0,0 +1,33 @@
+package com.slack.eatapp.controller;
+
+import com.slack.eatapp.model.Attachment;
+import com.slack.eatapp.model.TestResponse;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SlackController {
+
+    @PostMapping(value = "/slack/slash", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public TestResponse onReceiveSlashCommand(@RequestParam("team_id") String teamId,
+                                              @RequestParam("team_domain") String teamDomain,
+                                              @RequestParam("channel_id") String channelId,
+                                              @RequestParam("channel_name") String channelName,
+                                              @RequestParam("user_id") String userId,
+                                              @RequestParam("user_name") String userName,
+                                              @RequestParam("command") String command,
+                                              @RequestParam("text") String text,
+                                              @RequestParam("response_url") String responseUrl) {
+        TestResponse response = new TestResponse();
+        response.setText("This is the response text: " + channelId + " " + channelName + " " + userName);
+        response.setResponseType("in_channel");
+        Attachment attachment = new Attachment();
+        attachment.setText("This is the attachment text");
+        attachment.setColor("#0000ff");
+        response.getAttachments().add(attachment);
+        return response;
+    }
+
+}
