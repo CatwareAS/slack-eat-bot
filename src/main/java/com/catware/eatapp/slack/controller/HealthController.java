e9a6770b54ed61b1abaec57f93808e184083b766
Petro Krasnomovets <zhyrok@gmail.com>
2020-04-21 06:46:06 +0200

    Updating response to include timestamp

--- src/main/java/com/catware/eatapp/slack/controller/HealthController.java
+++ src/main/java/com/catware/eatapp/slack/controller/HealthController.java
@@ -5,13 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
+import java.time.LocalDateTime;
 
 @RestController
 public class HealthController {
 
     @GetMapping("/")
-    public ResponseEntity<String> handle() throws IOException {
-        return ResponseEntity.ok("UP");
+    public ResponseEntity<String> handle() {
+        return ResponseEntity.ok("UP :" + LocalDateTime.now());
     }
 }
